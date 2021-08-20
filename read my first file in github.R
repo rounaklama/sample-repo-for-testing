afc0d244cec8f28080fdd651801404bef2ff844d
rounaklama <rounaklamarl4@gmail.com>
2021-08-20 22:40:27 +0530

    my fisrt commit

---
+++ read my first file in github.R
@@ -0,0 +1,4 @@
+print("this file is created in r studio ")
+print("and explaining to connect git with rstudio")
+print("this is the process to check the connection between Rstudio and git") 
+print("by making changes in local computer after that by COMMIT and PUSH it to github which is online")
